ff6e8603e95968da93b3f210689902e3f3d7f982
Satvik Dasariraju <satvik.dasariraju@pennmedicine.upenn.edu>
2021-04-08 21:47:24 -0400

    Update RAREConstantBinSizeFunctionsDefinition.py

--- RAREConstantBinSizeFunctionsDefinition.py
+++ RAREConstantBinSizeFunctionsDefinition.py
@@ -1,5 +1,5 @@
-#This file contains all the code defining MODIFIED version of RARE with Constant Bin Size
-#No function is ever called in this file, please see the "RARE Experiment 7" file for examples of how to run RARE with Constant Bin Size on the Rare Variant Data Simulators
+#This file contains all the code defining the version of RARE with Constant Bin Size
+#No function is ever called in this file, please see the "RAREExperiment6" file for an example of how to run RARE with Constant Bin Size on the Rare Variant Data Simulators
 #Readers please note: amino acid refers to a rare variant feature, while amino acid bin is the equivalent of a bin of rare variant features
 
 
